99fbc2c507d98bbd12772b15c8bc80ac6ae23dac
lllnniii <inna.fox1000@gmail.com>
2026-03-29 03:06:30 +0300

    add structure

---
+++ backend/app/models/client.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from ..database import Base
+
+class Client(Base):
+    __tablename__ = "client"
+    client_id = Column(Integer, primary_key=True, index=True)
+    client_name = Column(String(255),index=True, unique=True, nullable=False)
+    full_name = Column(String(255), nullable=False, index=True, unique=True)
+    inn = Column(String(255), unique=True, nullable=False)
+    address_legal = Column(String(255))
+    address_actual = Column(String(255))
+    contact_person = Column(String(255))
+    contact_phone = Column(String(20))
+    contact_email = Column(String(255))
