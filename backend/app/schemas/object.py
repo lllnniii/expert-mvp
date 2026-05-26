1493d7db0b92dd368ab5be08f730f3e4f8141ffd
lllnniii <inna.fox1000@gmail.com>
2026-05-26 23:43:14 +0300

    issue (but i never mistake....)

--- backend/app/schemas/object.py
+++ backend/app/schemas/object.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+from app.schemas.client import ClientResponse
 
 class ObjectBase(BaseModel):
     client_id: int
@@ -12,9 +13,14 @@ class ObjectBase(BaseModel):
 class ObjectCreate(ObjectBase):
     pass
 
-class ObjectResponse(ObjectBase):
+class ObjectResponse(BaseModel):
     object_id: int
-
+    client_id: int
+    object_name: str = Field(..., max_length=255)
+    object_address: Optional[str] = None
+    opos_category: Optional[str] = None
+    description: Optional[str] = None
+    client: ClientResponse
     class Config:
         from_attributes = True
 
